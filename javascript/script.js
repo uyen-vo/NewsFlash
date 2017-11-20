d63ded7b9909a154999b326ec959dc11057b73c2
Uyen Vo <uyenvo@tamu.edu>
2017-11-19 22:29:41 -0600

    Added basic layout and animations

---
+++ javascript/script.js
@@ -0,0 +1,51 @@
+var about = false;
+var dev = false;
+
+function navClick( button ) {
+	if (button === 'about') {
+		if (!about){
+			var height = $( window ).height() - $("nav").height();
+			$(".about").height(height);
+			$(".left").addClass("active");
+			$(".about").animate({left: "0px"}, 500)
+			about = true;
+		}
+
+		else {
+			$(".about").animate({left: "-352px"}, 500)
+			$(".left").removeClass("active");
+			about = false;
+		}
+	}
+
+	if (button === 'dev') {
+		if (!dev){
+			var height = $( window ).height() - $("nav").height();
+			$(".dev").height(height);
+			$(".dev").animate({right: "0px"}, 500);
+			$(".right").addClass("active");
+			dev = true;
+		}
+
+		else {
+			$(".dev").animate({right: "-352px"}, 500);
+			$(".right").removeClass("active");
+			dev = false;
+		}
+	}
+
+	if (button === 'main') {
+		if (dev) {
+			$(".dev").animate({right: "-352px"}, 500);
+			$(".right").removeClass("active");
+			dev = false;
+		}
+
+		if (about) {
+			$(".about").animate({left: "-352px"}, 500)
+			$(".left").removeClass("active");
+			about = false;
+		}
+	}
+	
+}
